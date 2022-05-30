0ce5c7a735ca30b21a3714a323f5d664856fd1a4
kubersharma001 <kubersharma001@gmail.com>
2022-05-30 15:00:54 +0000

    payton: build: Build Lawnchair when available

--- cherish_payton.mk
+++ cherish_payton.mk
@@ -14,9 +14,10 @@
 # limitations under the License.
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common Sweet stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
